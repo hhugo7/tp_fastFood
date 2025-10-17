24a044646b3668f226dea56efaacbd7b113b88b1
FskiTeo <teo.fiminski@gmail.com>
2025-10-17 12:12:10 +0200

    question 3: Sum Up To + Doc

--- src/main/java/com/foodfast/utils/FoodFastUtils.java
+++ src/main/java/com/foodfast/utils/FoodFastUtils.java
@@ -2,6 +2,18 @@ package com.foodfast.utils;
 
 public class FoodFastUtils {
 
+    /**
+     * Constructor
+     */
+    public FoodFastUtils() {
+        return;
+    }
+
+    /**
+     * FizzBuzz function
+     * @param n input number
+     * @return fizz, buzz or fizzbuzz
+     */
     public static String deliveryPlanner(int n){
         String returnString = "";
 
@@ -16,5 +28,21 @@ public class FoodFastUtils {
         }
 
         return returnString;
+    }
+
+    /**
+     * Returns the sum up to a number
+     * @param n number to sum up to
+     * @return sum
+     */
+    public static int sumUpTo(int n) {
+
+        int result = 0;
+
+        for (int i = 0; i <= n; i++) {
+            result += i;
+        }
+
+        return result;
     }
 }
