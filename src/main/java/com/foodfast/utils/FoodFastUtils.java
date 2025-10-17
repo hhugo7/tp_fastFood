a22840dc753ff4f7a80a88a318931cee3304b40e
FskiTeo <teo.fiminski@gmail.com>
2025-10-17 11:58:13 +0200

    question 2: Devlivery Planner

--- src/main/java/com/foodfast/utils/FoodFastUtils.java
+++ src/main/java/com/foodfast/utils/FoodFastUtils.java
@@ -3,6 +3,18 @@ package com.foodfast.utils;
 public class FoodFastUtils {
 
     public static String deliveryPlanner(int n){
+        String returnString = "";
 
+        int modulo3 = n % 3;
+        if (modulo3 == 0) {
+            returnString += "Fizz";
+        }
+
+        int modulo5 = n % 5;
+        if (modulo5 == 0) {
+            returnString += "Buzz";
+        }
+
+        return returnString;
     }
 }
